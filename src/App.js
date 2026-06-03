d16ee79a17414afa019437beba775a491a53e35a
Fernandoezz <fernandomps.21@uom.lk>
2026-06-03 07:16:34 +0530

    update achievements

--- src/App.js
+++ src/App.js
@@ -139,7 +139,7 @@ const projects = [
 ];
 
 const achievements = [
-  { title: "Chathurmana", role: "Finalist", org: "University of Jayawardhanapura" },
+  { title: "Chathurmana", role: "Finalist", org: "University of Sri Jayewardenepura" },
   { title: "HackMoral 2024", role: "Participant", org: "University of Moratuwa" },
   { title: "Duothan 3.0 2023", role: "Participant", org: "NSBM" },
   { title: "Code Rush 2023", role: "Participant", org: "INTECS" },
